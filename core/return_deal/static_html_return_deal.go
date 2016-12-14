c7a89fa57f2e68dbd78caef8443e5dff95e19000
fudali113 <fudali113@gmail.com>
2016-12-14 16:15:16 +0800

    fix bug

--- core/return_deal/static_html_return_deal.go
+++ core/return_deal/static_html_return_deal.go
@@ -1,9 +1,12 @@
 package return_deal
 
 import (
+	"io/ioutil"
 	"log"
 	"net/http"
 	"strings"
+
+	"github.com/fudali113/doob/utils"
 )
 
 type staticHtmlReturnDeal struct {
@@ -14,8 +17,37 @@ func (*staticHtmlReturnDeal) MacthType(str string) bool {
 }
 
 //	实现 Dealer 接口
-func (*staticHtmlReturnDeal) Deal(returnType *ReturnType, res http.ResponseWriter) {
+func (*staticHtmlReturnDeal) Deal(returnType *ReturnType, w http.ResponseWriter) {
 	log.Print(returnType.TypeStr)
+	path := getPath(returnType.TypeStr)
+	data := returnType.Data
+	if data == nil {
+		bytes, err := ioutil.ReadFile(path)
+		if err != nil {
+			w.WriteHeader(500)
+			return
+		}
+		w.Write(bytes)
+	} else {
+		bytes, err := getTempleteBytes(path, data)
+		if err != nil {
+			w.WriteHeader(500)
+			return
+		}
+		w.Write(bytes)
+	}
+}
+
+func getPath(typeStr string) string {
+	typeAndPath := utils.Split(typeStr, ":")
+	if len(typeAndPath) == 2 {
+		return typeAndPath[1]
+	}
+	return ""
+}
+
+func getTempleteBytes(path string, data interface{}) ([]byte, error) {
+	return []byte{}, nil
 }
 
 func init() {
--- utils/reflect/reflect_test.go
+++ utils/reflect/reflect_test.go
@@ -9,13 +9,18 @@ func test(string, string, string, http.ResponseWriter) string {
 	return ""
 }
 
+func returnHtml() string {
+	return "static/index.html"
+}
+
 func Test_GetFuncParams(t *testing.T) {
-	params, returns := GetFuncParams(test)
+	params, returns := GetFuncParams(returnHtml)
 	logger.Debug("%v", params)
 	logger.Debug("%v", returns)
 	if len(params) != 4 {
 		t.Error("GetFuncParams have bug")
 	}
+	logger.Debug("------ %v", returns)
 	if len(returns) != 1 {
 		t.Error("GetFuncParams have bug")
 	}
