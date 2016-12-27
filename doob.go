0626ae0bf95ac6da99e745e7be5910ba0d52e43a
“fudali113” <fudali113@gmail.com>
2016-12-27 22:18:14 +0800

    delete my log impl

--- doob.go
+++ doob.go
@@ -6,11 +6,11 @@ package doob
 
 import (
 	"io/ioutil"
+	"log"
 	"net/http"
 	"strings"
 
 	"github.com/fudali113/doob/core"
-	"github.com/fudali113/doob/log"
 	"github.com/fudali113/doob/utils"
 )
 
@@ -25,16 +25,15 @@ const (
 )
 
 var (
-	logger          = log.GetLog("doob")
 	staticFileCache = map[string][]byte{}
 )
 
 // start doob server
 func Start(port int) {
-	logger.Info("server is starting , listen port is %d", port)
+	log.Printf("server is starting , listen port is %d", port)
 	err := core.Listen(port)
 	if err != nil {
-		logger.Error("start is fail => %s", err.Error())
+		log.Printf("start is fail => %s", err.Error())
 	}
 }
 
