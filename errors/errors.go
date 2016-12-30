79362568f23a0bd097d6378cba3df745c7b50ae3
fudali113 <fudali113@gmail.com>
2016-12-30 18:16:35 +0800

    complete err deal

--- errors/errors.go
+++ errors/errors.go
@@ -1,9 +1,10 @@
 package errors
 
 import (
-	"runtime/debug"
 	"net/http"
+	"runtime/debug"
 
+	"errors"
 	. "github.com/fudali113/doob/http_const"
 )
 
@@ -11,26 +12,29 @@ var (
 	errDealers = []ErrDealer{}
 )
 
-func AddErrDealer(errDealer ...ErrDealer)  {
+func AddErrDealer(errDealer ...ErrDealer) {
 	errDealers = append(errDealers, errDealer...)
 }
 
 // check panic err , match err and deal
-func CheckErr(err interface{}, w http.ResponseWriter, r *http.Request, isDev bool)  {
+func CheckErr(err interface{}, w http.ResponseWriter, r *http.Request, isDev bool) {
+
+	defer func() {
+		if err := recover(); err != nil {
+			// default err dealer
+			w.WriteHeader(INTERNAL_SERVER_ERROR)
+			if isDev {
+				WriteErrInfo(err, debug.Stack(), w)
+			}
+		}
+	}()
 
 	// traverse errDealers match true dealer and deal
-	for _,errDealer := range errDealers {
+	for _, errDealer := range errDealers {
 		if errDealer.Match(err) {
-			errDealer.Deal(err,w,r)
+			errDealer.Deal(err, w, r)
 			return
 		}
 	}
 
-	// default err dealer
-	w.WriteHeader(INTERNAL_SERVER_ERROR)
-	if isDev {
-		WriteErrInfo(err,debug.Stack(),w)
-	}
-
 }
-
